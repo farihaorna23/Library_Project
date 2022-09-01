60000169216538447aa14ffc30e9d5b22c36c155
farihaorna23 <farihaorna@gmail.com>
2022-09-01 17:02:34 -0400

    finished working on the project

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ let author = document.getElementById("author");
 let checkedBox = document.getElementById("read");
 let btn = document.getElementById("btn");
 let inputRow = document.getElementById("inputRow");
-
+let tableBody = document.querySelector("tbody");
+let allRows = document.querySelectorAll("tr");
 class Library {
   constructor() {
     //do we need to create a constructor?
@@ -44,19 +45,19 @@ class Library {
     this.updateBookList(addBook);
   }
 
-  removeBook(id) {
+  removeBook(id, row) {
     console.log(id);
     console.log(this.books);
     this.books = this.books.filter(book => book.id !== id);
     console.log(this.books);
-    for (let i = 0; i < this.books.length; i++) {
-      this.updateBookList(this.books[i]);
-    }
+    console.log(row);
+    row.remove();
   }
 
   //recives one book obj
   updateBookList(obj) {
-    console.log(obj);
+    console.log(obj); //but I had to pass this obj from addBook for updateBookList. This obj can't be accessed from anywhere. Because it is a local value.
+    console.log(this.books); // i don't need to pass this.books as an argument. I can access them because they r the class proprty that can be accepted from anywhere.
     let newRow = document.createElement("tr");
     let newTitle = document.createElement("td");
     let newAuthor = document.createElement("td");
@@ -66,7 +67,7 @@ class Library {
     let rmButtonCol = document.createElement("td");
     let removeBook = document.createElement("button");
     removeBook.textContent = "Remove Book";
-    removeBook.addEventListener("click", () => this.removeBook(obj.id));
+    removeBook.addEventListener("click", () => this.removeBook(obj.id, newRow)); //passed by reference
     rmButtonCol.appendChild(removeBook);
     newTitle.textContent = obj.title;
     newAuthor.textContent = obj.author;
